516867088714c28b5e6fd325c87e4777cd0875e4
Fortern <blueten.ki@gmail.com>
2022-10-20 16:25:30 +0800

    打印类名 打印异常

--- src/test/kotlin/xyz/fortern/simpletest/TestA.java
+++ src/test/kotlin/xyz/fortern/simpletest/TestA.java
@@ -2,6 +2,8 @@ package xyz.fortern.simpletest;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Consumer;
+
 public class TestA {
 	@Test
 	void testBinary() {
@@ -19,5 +21,39 @@ public class TestA {
 	@Test
 	void stringWithNull() {
 		System.out.println("abc" + null + "def");
+	}
+	
+	@Test
+	void printException() {
+		try {
+			throw new RuntimeException("手动抛出异常");
+		} catch (RuntimeException e) {
+			System.out.println(e);//异常堆栈的第一行信息，包含异常类型与描述信息
+			System.out.println("======");
+			System.out.println(e.getMessage());//异常的描述信息
+			System.out.println("======");
+			e.printStackTrace();//异常的完整信息
+		}
+	}
+	
+	@Test
+	void printClassName() {
+		Consumer<String> consumer = new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		};
+		Class<? extends Consumer> clazz = consumer.getClass();
+		System.out.println(clazz);//class xyz.fortern.simpletest.TestA
+		System.out.println("======");
+		//全类名
+		System.out.println(clazz.getName());//xyz.fortern.simpletest.TestA
+		System.out.println("======");
+		//不包含包路径的类名如果是匿名内部类则为空字符串
+		System.out.println(clazz.getSimpleName());//TestA
+		System.out.println("======");
+		//标准名称，如果是匿名内部类则为null
+		System.out.println(clazz.getCanonicalName());//xyz.fortern.simpletest.TestA
 	}
 }
