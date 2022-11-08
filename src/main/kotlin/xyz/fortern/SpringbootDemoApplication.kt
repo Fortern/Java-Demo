8b894d1d7952a753fe2d2acd5af39885a77b0a21
Fortern <blueten.ki@gmail.com>
2022-11-08 14:57:16 +0800

    数据库支持

--- src/main/kotlin/xyz/fortern/SpringbootDemoApplication.kt
+++ src/main/kotlin/xyz/fortern/SpringbootDemoApplication.kt
@@ -1,11 +1,13 @@
 package xyz.fortern
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@MapperScan("xyz.fortern.dao")
 open class SpringbootDemoApplication
 
 // 主函数，启动类
