35d7111e181de49e77a00acf4ec65b74b3843c26
Fortern <blueten.ki@gmail.com>
2023-01-06 15:10:51 +0800

    类名输出测试

--- src/test/kotlin/xyz/fortern/simpletest/TestA.java
+++ src/test/kotlin/xyz/fortern/simpletest/TestA.java
@@ -3,6 +3,7 @@ package xyz.fortern.simpletest;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class TestA {
 	@Test
@@ -38,22 +39,46 @@ public class TestA {
 	
 	@Test
 	void printClassName() {
-		Consumer<String> consumer = new Consumer<String>() {
+		Consumer<String> consumer = new Consumer<>() {
 			@Override
 			public void accept(String s) {
 				System.out.println(s);
 			}
 		};
-		Class<? extends Consumer> clazz = consumer.getClass();
-		System.out.println(clazz);//class xyz.fortern.simpletest.TestA
-		System.out.println("======");
+		Class<?> clazz1 = consumer.getClass();
+		System.out.println("匿名内部类测试");
+		//toString
+		System.out.println("toString(): " + clazz1);//class xyz.fortern.simpletest.TestA$1
 		//全类名
-		System.out.println(clazz.getName());//xyz.fortern.simpletest.TestA
-		System.out.println("======");
-		//不包含包路径的类名如果是匿名内部类则为空字符串
-		System.out.println(clazz.getSimpleName());//TestA
-		System.out.println("======");
+		System.out.println("getName()" + clazz1.getName());//xyz.fortern.simpletest.TestA$1
+		//类的名称，如果是匿名内部类则为空字符串
+		System.out.println("getSimpleName()" + clazz1.getSimpleName());//
 		//标准名称，如果是匿名内部类则为null
-		System.out.println(clazz.getCanonicalName());//xyz.fortern.simpletest.TestA
+		System.out.println("getCanonicalName()" + clazz1.getCanonicalName());//null
+		
+		System.out.println("=================");
+		System.out.println("Lambda测试");
+		Function<String, String> function = a -> a;
+		Class<?> clazz2 = function.getClass();
+		//toString
+		System.out.println("toString(): " + clazz2);//class xyz.fortern.simpletest.TestA$$Lambda$343/0x0000000800ca27b8
+		//全类名
+		System.out.println("getName(): " + clazz2.getName());//xyz.fortern.simpletest.TestA$$Lambda$343/0x0000000800ca27b8
+		//类的名称，如果是Lambda则为空字符串
+		System.out.println("getSimpleName(): " + clazz2.getSimpleName());//TestA$$Lambda$343/0x0000000800ca27b8
+		//标准名称，如果是Lambda则为null
+		System.out.println("getCanonicalName(): " + clazz2.getCanonicalName());//null
+		
+		System.out.println("=================");
+		System.out.println("普通类测试");
+		Class<TestA> clazz3 = TestA.class;
+		//toString
+		System.out.println("toString(): " + clazz3);//class xyz.fortern.simpletest.TestA
+		//全类名
+		System.out.println("getName(): " + clazz3.getName());//xyz.fortern.simpletest.TestA
+		//类的简单名称
+		System.out.println("getSimpleName(): " + clazz3.getSimpleName());//TestA
+		//标准名称
+		System.out.println("getCanonicalName(): " + clazz3.getCanonicalName());//xyz.fortern.simpletest.TestA
 	}
 }
