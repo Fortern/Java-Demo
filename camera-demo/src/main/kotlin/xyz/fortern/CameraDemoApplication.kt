097f813387f947b926e85208853efa6cc94724e3
Fortern <blueten.ki@gmail.com>
2023-02-28 11:00:11 +0800

    配置Nacos

--- camera-demo/src/main/kotlin/xyz/fortern/CameraDemoApplication.kt
+++ camera-demo/src/main/kotlin/xyz/fortern/CameraDemoApplication.kt
@@ -3,8 +3,10 @@ package xyz.fortern
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @MapperScan("xyz.fortern.mapper")
+@EnableDiscoveryClient
 @SpringBootApplication
 class CameraDemoApplication
 
